237cc0c51a6c6145636d43045aa082ffb349c37a
tadashiro1 <lucasmacielg73@gmail.com>
2023-11-10 14:04:07 -0400

    Programa Doninha LCP-ATIV1

---
+++ dawkins_weasel_problem.py
@@ -0,0 +1,5 @@
+"""
+we will make a code that generate different phrases using the allowed characters until make the expected phrase
+ALLOWED CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' , 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+TARGET STRING = 'ME THINKS IT IS LIKE A WEASEL'
+"""
