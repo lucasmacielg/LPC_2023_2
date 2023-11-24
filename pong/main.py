0ba3965bed3bd23a277b929cc88c128bf00f2497
lucasmacielg <lmg.snf23@uea.edu.br>
2023-11-23 22:15:43 -0400

    bug restart

--- pong/main.py
+++ pong/main.py
@@ -83,19 +83,20 @@ def draw(screen, paddles, ball, left_score, right_score):
     pygame.display.update()
 
 
-def handle_collision(ball, left_paddle, right_paddle):
-    bounce_sound_effect = pygame.mixer.Sound('assets/bounce.wav')
-
+def collision(ball, left_paddle, right_paddle):
+    bounce_sound = pygame.mixer.Sound('assets/bounce.wav')
     if ball.y + ball.radius >= HEIGHT:
         ball.y_vel *= -1
     elif ball.y - ball.radius <= 0:
         ball.y_vel *= -1
 
+
     if ball.x_vel < 0:
         if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
             if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
                 ball.x_vel *= -1
-                bounce_sound_effect.play()
+                bounce_sound.play()
+
                 middle_y = left_paddle.y + left_paddle.height / 2
                 difference_in_y = middle_y - ball.y
                 reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL
@@ -106,13 +107,13 @@ def handle_collision(ball, left_paddle, right_paddle):
         if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
             if ball.x + ball.radius >= right_paddle.x:
                 ball.x_vel *= -1
-                bounce_sound_effect.play()
-                middle_y = left_paddle.y + left_paddle.height / 2
+                bounce_sound.play()
+
+                middle_y = right_paddle.y + right_paddle.height / 2
                 difference_in_y = middle_y - ball.y
-                reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL
+                reduction_factor = (right_paddle.height / 2) / ball.MAX_VEL
                 y_vel = difference_in_y / reduction_factor
                 ball.y_vel = -1 * y_vel
-
 
 
 def movement(keys, left_paddle, right_paddle):
@@ -182,7 +183,7 @@ def main():
         movement(keys, left_paddle, right_paddle)
 
         ball.move()
-        handle_collision(ball, left_paddle, right_paddle)
+        collision(ball, left_paddle, right_paddle)
 
         if ball.x < 0 and ball.x_vel < 0:
             right_score += 1
